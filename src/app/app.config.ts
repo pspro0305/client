1c1d8c957c8631d29eba63ef0b33b01002c9553e
pspro0305 <pspro0305@gmail.com>
2025-02-27 12:05:06 +0530

    End of Section 6

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,12 +4,19 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideToastr } from 'ngx-toastr';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
      provideRouter(routes),
      provideHttpClient(),  // Provider added for HTTP client API calls
-     provideAnimations()
+     provideAnimations(),
+     provideToastr({
+        timeOut: 3000,
+        positionClass: 'toast-bottom-right',
+        preventDuplicates: true,
+        closeButton: true
+     }) // Provider added for Toastr notifications
     ]
 };
--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,31 +1,35 @@
 import { Component, inject } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import { AccountService } from '../_Services/account.service';
-import { NgIf } from '@angular/common';
+import { NgIf, TitleCasePipe } from '@angular/common';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [FormsModule, NgIf, BsDropdownModule],
+  imports: [FormsModule, NgIf, BsDropdownModule, RouterLink, RouterLinkActive, TitleCasePipe],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
     public accountService = inject(AccountService);
+    private router = inject(Router);
+    private toaster = inject(ToastrService);
       model : any ={};
       login(){
        this.accountService.login(this.model)
        .subscribe({
-        next: response => {
-          console.log(response);
-        },
+        next: _ => this.router.navigateByUrl('/members'),
         error: error => {
           console.log(error);
+          this.toaster.error(error.error);
         }
       });     
 }
 logout(){
   this.accountService.logout();
+  this.router.navigateByUrl('/');
 }
 }
